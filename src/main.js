773ee611bd6e409779c12fecef2ca96bd7b7765a
Madison Hardt <madisonmonteze@gmail.com>
2021-09-27 11:22:31 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import "./styles/main.scss";
 import "./scripts/utils/vh";
 
 console.groupCollapsed("Site Credits");
-console.log("Development - Madison Hardt https://www.madisonmonteze.studio/ for Human NYC");
+console.log("Development - Madison Hardt https://www.madisonmonteze.studio/");
 console.groupEnd();
